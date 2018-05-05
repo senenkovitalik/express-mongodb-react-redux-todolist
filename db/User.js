844406889bb59ac6903eeeacb91cee56d7808012
Senenko Vitaliy <senenkovitalik@gmail.com>
2018-05-06 00:12:33 +0300

    feat: DB UserShema with password hashing and authentication method

---
+++ db/User.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 20
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    tolowercase: true,
+    index: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 8,
+    maxlength: 25
+  },
+  created_at: {
+    type: Date
+  },
+  modified_at: {
+    type: Date,
+    default: Date.now
+  },
+  task_lists: []
+});
+
+UserSchema.pre('save', function (next) {
+  const user = this;
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
+});
+
+UserSchema.statics.authenticate = (email, password, callback) => {
+  User.findOne({ email })
+    .exec((err, user) => {
+      if (err) {
+        return callback(err);
+      } else if (!user) {
+        const err = new Error("User not found");
+        err.status = 401; // Unauthorized
+        return callback(err);
+      }
+
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      })
+    })
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
