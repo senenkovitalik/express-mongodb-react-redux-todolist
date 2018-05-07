a2109196e63ff1650ede8c963041be7faeab71fe
Senenko Vitaliy <senenkovitalik@gmail.com>
2018-05-07 13:59:02 +0300

    chore: auto-rerun tests

---
+++ db/Task.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  modified_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Task = mongoose.model('Task', TaskSchema);
+
+module.exports = {
+  TaskSchema,
+  Task
+};
