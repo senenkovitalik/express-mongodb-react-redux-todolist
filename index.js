b6bba09a336d88bcbb242dee30995583d8c2f8c0
Senenko Vitaliy <senenkovitalik@gmail.com>
2018-05-12 00:17:31 +0300

    feat: project entry point
    1) express
    2) body parser
    3) mongoose
    4) session - user tracking
    5) Mongo Store - save user session into DB

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+mongoose.set('debug', true);
+mongoose.connect('mongodb://localhost/test');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("Successfully connected to MongoDB");
+});
+
+const app = express();
+
+app.use(session({
+  secret: 'vEry_$tr0ng-P@$$',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const routes = require('./routes/router');
+app.use("/api", routes);
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
