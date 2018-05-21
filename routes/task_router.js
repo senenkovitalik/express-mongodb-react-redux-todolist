9712f9aa0bbffed85fe80146cdb61c5a05518b4a
Senenko Vitaliy <senenkovitalik@gmail.com>
2018-05-21 17:56:24 +0300

    feat: create task

--- routes/task_router.js
+++ routes/task_router.js
@@ -3,24 +3,21 @@ const taskRouter = express.Router({ mergeParams: true });
 
 const User = require('../db/User');
 
-const { handleMongooseError } = require('../utils');
+const { findUser, handleMongooseError } = require('../utils');
 
 // create task
-taskRouter.post('/tasks/:title', (req, res) => {
+taskRouter.post('/:title', findUser, (req, res) => {
   const { id, title } = req.params;
+  const { user } = req;
 
-  let taskId = null;
+  const list = user.task_lists.id(id);
+  const task = list.tasks.create({ title });
+  const taskId = task._id;
 
-  User.findById(req.session.user_id)
-    .then(user => {
-      const list = user.task_lists.id(id);
-      const task = list.tasks.create({ title });
-      taskId = task._id;
-      list.push(task);
+  list.tasks.push(task);
 
-      return user.save();
-    })
-    .then(() => {
+  user.save()
+    .then((user) => {
       res.location(`/api/lists/${id}/tasks/${taskId}`);
       res.status(201).end();
     })
