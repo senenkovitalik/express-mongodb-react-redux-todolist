46d383ff240435b6649388b046e07e4236d8dd7b
Senenko Vitaliy <senenkovitalik@gmail.com>
2018-05-21 22:50:17 +0300

    feat: get task(s)

--- routes/task_router.js
+++ routes/task_router.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const taskRouter = express.Router({ mergeParams: true });
 
-const User = require('../db/User');
-
 const { findUser, handleMongooseError } = require('../utils');
+
+// get list of tasks
+taskRouter.get('/', findUser, (req, res) => {
+  const { user } = req;
+  const { id } = req.params;
+
+  const task_list = user.task_lists.id(id);
+
+  if (task_list !== null) {
+    const { tasks } = task_list;
+
+    if (tasks) {
+      res.status(200).json(tasks);
+    } else {
+      res.status(404).end();
+    }
+  } else {
+    res.status(404).end();
+  }
+});
+
+// get task by ID
+taskRouter.get('/:taskId', findUser, (req, res) => {
+  const { user } = req;
+  const { id: listId, taskId } = req.params;
+
+  const task = user.task_lists.id(listId).tasks.id(taskId);
+
+  if (task) {
+    res.status(200).json(task);
+  } else {
+    res.status(404).end();
+  }
+});
 
 // create task
 taskRouter.post('/:title', findUser, (req, res) => {
