76c6d06973e90854047bda56cb106bfb25d5d54b
Senenko Vitaliy <senenkovitalik@gmail.com>
2018-08-20 22:11:46 +0300

    refactor: Task layout

--- client/src/content/tasks/Task.js
+++ client/src/content/tasks/Task.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {
     Card,
-    CardBody
+    CardBody,
+  Input
 } from 'reactstrap';
+import './task.css';
 
 class Task extends React.Component {
     constructor(props) {
@@ -11,14 +13,16 @@ class Task extends React.Component {
 
     render() {
         return (
-            <Card className="task">
-                <CardBody className="d-flex flex-row" style={{padding: 0.5+'rem'}}>
-                    <div>
-                        <input type="checkbox" className="mr-2" />
-                    </div>
-                    <div className="d-flex flex-column">
-                        <div>Write programs</div>
-                        <small id="emailHelp" className="form-text text-primary">Tomorrow, 9:00</small>
+            <Card className="task w-100">
+                <CardBody>
+                    <div className="form-check">
+                      <Input className="form-check-input" type="checkbox"/>
+                      <label className="mb-0">
+                        <span className="d-flex flex-column task-description">
+                          <span>Task #</span>
+                          <span className="text-dark"><small>Tue, 1, Aug. 2018, 10:00</small></span>
+                        </span>
+                      </label>
                     </div>
                 </CardBody>
             </Card>
