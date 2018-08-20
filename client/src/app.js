9f0c089e99560287535f88544649e06494ac9c8b
Senenko Vitaliy <senenkovitalik@gmail.com>
2018-08-20 23:18:40 +0300

    feat: add router

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import Header from "./Header";
 import Footer from "./Footer";
+import Home from './content/home/Home';
 import Tasks from "./content/tasks/Tasks";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
     constructor(props) {
@@ -12,11 +16,16 @@ class App extends React.Component {
 
     render() {
         return (
-          <div>
-            <Header />
-            <Tasks />
-            <Footer />
-          </div>
+          <Router>
+            <div>
+              <Header />
+
+              <Route exact path="/" component={Home} />
+              <Route path="/lists" component={Tasks} />
+
+              <Footer />
+            </div>
+          </Router>
         )
     }
 }
