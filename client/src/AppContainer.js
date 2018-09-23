21fe0fbcd7e0f525b41b3cb774851e50d6ed6d4d
Senenko Vitaliy <senenkovitalik@gmail.com>
2018-09-23 18:21:34 +0300

    fix: App must contain Redux Store

---
+++ client/src/AppContainer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { todoApp } from './redux/reducers';
+import App from './App';
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  todoApp,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+);
+
+export const AppContainer = () => {
+  return <Provider store={store}>
+            <App />
+        </Provider>
+};
+
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import { todoApp } from './redux/reducers';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-
-const loggerMiddleware = createLogger();
-
-const store = createStore(
-  todoApp,
-  applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware
-  )
-);
+import {AppContainer} from "./AppContainer";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>  ,
+  <Router>
+    <AppContainer />
+  </Router>,
   document.getElementById('app')
 );
