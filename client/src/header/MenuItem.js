7d30bb590e3a5e01c165d99e73a392bde6fc441a
Senenko Vitaliy <senenkovitalik@gmail.com>
2018-09-29 15:10:22 +0300

    fix: auto-close navbar when link is clicked

--- client/src/header/MenuItem.js
+++ client/src/header/MenuItem.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { NavItem, NavLink } from 'reactstrap';
 import {withRouter} from 'react-router';
 
-const MenuItem = ({ to, title, onClickAction, history, location }) => {
+const MenuItem = ({ to, title, onClickAction, history, location: { pathname } }) => {
+  const isActive = (to === pathname)
+    ? true
+    : (to !== "/")
+      ? pathname.startsWith(to)
+      : false;
+
   return (
     <NavItem>
-      <NavLink active={location.pathname === to} onClick={() => {
+      <NavLink active={isActive} onClick={() => {
         history.push(to);
         onClickAction();
       }}>{title}</NavLink>
