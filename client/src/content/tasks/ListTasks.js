69cb140053f71689e76359f3c647eeb7ee2c335a
Senenko Vitaliy <senenkovitalik@gmail.com>
2018-11-09 18:32:22 +0200

    feat: group tasks by period

--- client/src/content/tasks/ListTasks.js
+++ client/src/content/tasks/ListTasks.js
@@ -21,6 +21,7 @@ class ListTasks extends React.Component {
     };
 
     this.setCurrentList = this.setCurrentList.bind(this);
+    this.filterByDate = this.filterByDate.bind(this);
   }
 
   setCurrentList(id) {
@@ -28,12 +29,105 @@ class ListTasks extends React.Component {
     this.setState({ currentList: id });
   }
 
+  filterByDate(ps, tasks, period) {
+
+    let start, end, condition;
+
+    switch (period) {
+      case 'missed':
+        start     = ps.today.start;
+        condition = (start, end, dueDate) => dueDate.getTime() < start.getTime();
+        break;
+      case 'later':
+        end       = ps.next_month.end;
+        condition = (start, end, dueDate) => dueDate.getTime() > end.getTime();
+        break;
+      default:
+        start     = ps[period].start;
+        end       = ps[period].end;
+        condition = (start, end, dueDate) =>
+          dueDate.getTime() >= start.getTime() && dueDate.getTime() <= end.getTime();
+    }
+
+    return tasks.filter(t => {
+      const dueDate = new Date(t.dueDate);
+
+      return !isNaN(dueDate.getTime())
+        ? condition(start, end, dueDate)
+        : false;
+    });
+  }
+
   render() {
 
-    const current_list = this.props.lists[this.state.currentList];
+    const current_list = this.props.lists[this.state.currentList]
+      ? this.props.lists[this.state.currentList]
+      : [];
+
     const current_tasks = current_list.hasOwnProperty('tasks')
       ? Object.values(this.props.tasks).filter(task => current_list.tasks.includes(task._id))
       : [];
+
+    periods.create(new Date());
+    const ps = periods.computePeriods();
+    const p = {
+      0: {
+        name: 'Missed',
+        tasks: []
+      },
+      1: {
+        name: 'Today',
+        tasks: []
+      },
+      2: {
+        name: 'Tomorrow',
+        tasks: []
+      },
+      3: {
+        name: 'End of Week',
+        tasks: []
+      },
+      4: {
+        name: 'Next Week',
+        tasks: []
+      },
+      5: {
+        name: 'End of Month',
+        tasks: []
+      },
+      6: {
+        name: 'Next Month',
+        tasks: []
+      },
+      7: {
+        name: 'Later',
+        tasks: []
+      },
+      8: {
+        name: 'Without date',
+        tasks: []
+      },
+    };
+
+    p['0'].tasks = this.filterByDate(ps, current_tasks, 'missed');
+    p["1"].tasks = this.filterByDate(ps, current_tasks, 'today');
+    p["2"].tasks = this.filterByDate(ps, current_tasks, 'tomorrow');
+    p["3"].tasks = this.filterByDate(ps, current_tasks, 'end_of_week');
+    p["4"].tasks = this.filterByDate(ps, current_tasks, 'next_week');
+    p["5"].tasks = this.filterByDate(ps, current_tasks, 'end_of_month');
+    p["6"].tasks = this.filterByDate(ps, current_tasks, 'next_week');
+    p["7"].tasks = this.filterByDate(ps, current_tasks, 'later');
+    // Maybe I'll fix this. In future. Maybe)
+    p["8"].tasks = current_tasks.filter(t => t.dueDate === "null");
+
+    const tasks_to_show = Object.values(p).map((e, i) =>
+      e.tasks.length > 0
+        ? <React.Fragment key={i}>
+            <div key={i} className={`section-name ${e.name === 'Missed' ? 'text-danger' : 'text-primary'} font-weight-bold`}>{e.name}</div>
+            { e.tasks.map(task => <TaskItem key={task._id} task={task} />) }
+          </React.Fragment>
+        : null
+    );
 
     return (
       <Row noGutters className="justify-content-center">
@@ -52,17 +146,9 @@ class ListTasks extends React.Component {
 
           <div className="mb-3">
 
-            {
-              current_tasks.map(task => <TaskItem key={task._id} task={task} />)
-            }
+            // todo implement visibility filter
+            { tasks_to_show }
 
-            {/*// todo filter tasks by date*/}
-            <div className="section-name text-danger font-weight-bold">Missed</div>
-            <div className="section-name text-primary font-weight-bold">Today</div>
-            <div className="section-name text-primary font-weight-bold">Tomorrow</div>
-            <div className="section-name text-primary font-weight-bold">Next week</div>
-            <div className="section-name text-primary font-weight-bold">Next month</div>
-            <div className="section-name text-primary font-weight-bold">Later</div>
           </div>
 
           <NewTaskButton list={this.state.currentList} />
